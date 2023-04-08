4c9bfa704085340cfadf34fc3b7a96372f951455
joao_ <victorsilva428@gmail.com>
2023-04-08 15:26:52 -0300

    inital commit

--- lib/Game.dart
+++ lib/Game.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 
 class game extends StatefulWidget {
-  const game({Key? key}) : super(key: key);
+  String valor;
+
+  game(this.valor, {super.key});
 
   @override
   State<game> createState() => _gameState();
 }
 
 class _gameState extends State<game> {
+
+  void _voltar() {
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var caminhoImagem = "images/moeda_cara.png";
+    if (widget.valor == "cara") {
+      caminhoImagem = "images/moeda_cara.png";
+    }else{
+      caminhoImagem = "images/moeda_coroa.png";
+    }
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(caminhoImagem),
+            GestureDetector(
+              onTap: _voltar,
+              child: Image.asset("images/botao_voltar.png"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:cara_ou_coroa_flutter/Game.dart';
 import 'package:flutter/material.dart';
 
 class home extends StatefulWidget {
@@ -8,12 +11,33 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
+  void _exibir() {
+
+   var itens = ["cara", "coroa"];
+   var numero = Random().nextInt(itens.length);
+
+    Navigator.push(context,
+        MaterialPageRoute(builder:
+            (context) => game(itens[numero]))
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Color(0xff61bd86),
       body: Container(
-        
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("images/logo.png"),
+            GestureDetector(
+              onTap: _exibir,
+              child: Image.asset("images/botao_jogar.png"),
+            )
+          ],
+        ),
       ),
     );
   }
