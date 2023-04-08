e660d98cb9ee035a8abdb38772ce5b2fcdd73b0f
joao_ <victorsilva428@gmail.com>
2023-04-08 15:09:27 -0300

    Initial commit

---
+++ lib/Game.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class game extends StatefulWidget {
+  const game({Key? key}) : super(key: key);
+
+  @override
+  State<game> createState() => _gameState();
+}
+
+class _gameState extends State<game> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:cara_ou_coroa_flutter/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: home(),
+  ));
+}
